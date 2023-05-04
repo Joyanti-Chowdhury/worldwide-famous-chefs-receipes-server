469bb1fd3e01b5ff6be2d0d7e88095e786221d48
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-05-04 14:36:24 +0600

    specific chefs id implemented

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ app.get('/chef',(req,res)=>{
     res.send(chef);
 })
 
+app.get('/chef/:id',(req, res)=>{
+            const id = req.params.id;
+            console.log(id);
+            const selectedChef = chef.find(c => c._id === id);
+            res.send(selectedChef);
+
+})
+
+
+
+
 app.listen(port,() =>{
     console.log(`Are you ready API is running:${port}`);
 })
