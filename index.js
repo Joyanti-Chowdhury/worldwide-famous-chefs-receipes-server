d47934b0d74f3576af7dde02bba32d2d75a74b51
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-05-11 11:19:21 +0600

    reciepes data added

--- index.js
+++ index.js
@@ -57,14 +57,30 @@ const cors =require("cors");
 const app = express();
 app.use(cors());
 const allChefsData = require("./data/chef.json")
+const chef = require('./data/chef.json')
 
 
 app.get('/',(req,res)=>{
-    res.json({message:'helllo'})
+    res.json({message:'hello'})
 })
+
+
 
 app.get("/alldata",(req,res) => {
    res.send(allChefsData)
+
+})
+
+
+app.get('/chef',(req,res) =>{
+    res.send(chef);
+})
+
+app.get('/chef/:id',(req,res) =>{
+        const id = req.params.id;
+        console.log(id);
+        const selectedChef = chef.find(c=>c._id === id);
+        res.send(selectedChef)
 
 })
 app.listen(5000,() => {
